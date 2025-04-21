31d3a62f2fc366151b569085ca683cc8f30cc3e5
D2Mark <maktabi876@gmail.com>
2025-04-22 00:34:52 +0330

    fix: added export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import type { paths as clientPaths } from './api-client-v1'
+import type { paths as clientV2Paths } from './api-client-v2'
+import type { paths as dashboardPaths } from './api-dashboard'
+
 export * from './ws'
 export * from './http'
 export * from './config'
 export * from './api-dashboard.d'
+
+export type { clientPaths, clientV2Paths, dashboardPaths }
