8a38c1bf2ef7b2b8690bb72d57c8bf9ac08cfbc4
D2Mark <maktabi876@gmail.com>
2025-04-22 00:39:37 +0330

    fix: export the correct components

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-import type { paths as clientPaths } from './api-client-v1'
-import type { paths as clientV2Paths } from './api-client-v2'
-import type { paths as dashboardPaths } from './api-dashboard'
+import type {
+  paths as clientPaths,
+  components as clientComponents
+} from './api-client-v1'
+import type {
+  paths as clientV2Paths,
+  components as clientV2Components
+} from './api-client-v2'
+import type {
+  paths as dashboardPaths,
+  components as dashboardComponents
+} from './api-dashboard'
 
 export * from './ws'
 export * from './http'
 export * from './config'
 export * from './api-dashboard.d'
 
-export type { clientPaths, clientV2Paths, dashboardPaths }
+export type {
+  clientPaths,
+  clientV2Paths,
+  dashboardPaths,
+  clientComponents,
+  clientV2Components,
+  dashboardComponents
+}
